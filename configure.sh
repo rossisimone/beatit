afd52e82a906f0d261594fc49416fb42a2d25153
srossi <srossi@aorta.amath.unc.edu>
2016-08-24 17:30:30 -0400

    Configuration file example updated

--- configure.sh
+++ configure.sh
@@ -1,10 +1,58 @@
+rm -rf CMake*
+
+# BUILD MODE
+BUILD=RELEASE
 
 # Source directory of BeatIt. Use full Path
 BeatIt_SOURCE_DIR=
 
 # Install directory
-PREFIX=
+PREFIX=$BeatIt_SOURCE_DIR/../install
+
+# MPI
+MPICC=mpicc
+MPICXX=mpicxx
+
+# If you have one, this can be useful
+# TPL Base Folder
+# Example
+# TPL=~/TPL
+TPL=
+
+# LibMesh
+# Example: LIBMESH=$TPL
+# LIBMESH=$TPL/libmesh/1.0.0/
+LIBMESH=
+
+# VTK
+# Example
+# VTK=$TPL/vtk/7.0.0/lib/cmake/vtk-7.0
+VTK=
+
+# Boost
+# Example
+# BOOST=$TPL/boost/1.61.0/
+BOOST=
+
+# OPTIONS
+VERBOSITY=ON
+
 
 cmake \
-  -D CMAKE_ISNTALL_PREFIX=$PREFIX \
+  -D CMAKE_INSTALL_PREFIX=$PREFIX \
+  -D CMAKE_BUILD_TYPE=$BUILD \
+  -D BUILD_SHARED_LIBS=ON \
+  -D CMAKE_C_COMPILER=$MPICC \
+  -D CMAKE_CXX_COMPILER=$MPICXX \
+  -D CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++11" \
+  -D LIBMESH_DIR=$LIBMESH  \
+  -D VTK_DIR=$VTK  \
+  -D BOOST_ROOT=$BOOST \
+  -D CMAKE_VERBOSE_MAKEFILE=$VERBOSITY \
   $BeatIt_SOURCE_DIR
+
+
+
+
+
+
